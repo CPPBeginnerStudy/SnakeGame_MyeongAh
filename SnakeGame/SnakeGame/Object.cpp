f78c5d1d041d1fbf8608915195b16dc7c6341e1c
Subin Park <dlakek@naver.com>
2018-12-10 00:37:07 +0900

    02_과제_오브젝트 속도 추가 - 코멘트

--- SnakeGame/SnakeGame/Object.cpp
+++ SnakeGame/SnakeGame/Object.cpp
@@ -55,8 +55,13 @@ void Object::Update()
 	}
 	if (m_VelFlagX)
 	{
-		// 랜덤값 사용을 위해 랜덤시드를 초기화
-		srand((unsigned int)time(nullptr));
+        /// > 제가 srand()에 대한 설명이 부족했었네요.
+        /// > srand()는 rand()사용할 때마다 호출하는 것이 아니라
+        /// > 게임 시작시 딱 한번만 호출되어야 합니다. (GameManager::Init()에서)
+        /// > srand()가 매번 호출되면 그 때마다 랜덤시드(랜덤값의 기반)이 갱신되어서
+        /// > 모든 rand()가 동일한 랜덤값을 반환하게 되어버립니다.
+        /// > srand()는 rand()함수가 값을 산출할 때 참고하는 어떤 기반값을 설정하는 함수입니다.
+		//srand((unsigned int)time(nullptr));
 
 		m_VelocityX = (rand() % 3) + 1;
 		m_VelFlagX = false;
@@ -84,8 +89,7 @@ void Object::Update()
 	}
 	if (m_VelFlagY)
 	{
-		// 랜덤값 사용을 위해 랜덤시드를 초기화
-		srand((unsigned int)time(nullptr));
+		//srand((unsigned int)time(nullptr));
 
 		m_VelocityY = (rand() % 3) + 1;
 		m_VelFlagY = false;
