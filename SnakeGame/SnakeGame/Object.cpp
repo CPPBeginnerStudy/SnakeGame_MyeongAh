2bf163ae306acdec9e93363ff656bc62fe3bc7d8
MyeongAh <mohyun1004@ajou.ac.kr>
2019-02-26 23:36:44 +0900

    04_좌표 계산 코드 개선
    cmd 좌표계에서 x가 y의 절반이므로, x에 2를 곱하는 시점을 조금 더 알맞게 변경

--- SnakeGame/SnakeGame/Object.cpp
+++ SnakeGame/SnakeGame/Object.cpp
@@ -62,23 +62,32 @@ void Object::Render()
 	// 현재 내 위치(m_X, m_Y)에 내 모습(m_Shape)을 그리도록 한다.
 	// 실수형 변수로 바꾸어 주었지만, 어차피 도트 겜이라 좌표는 실수가 나올 수 없어서
 	// 적당히 short로 형변환 해서 Print에 넣어준다. (print가 받는 인자도 실수형이 아님)
-	Console::GetInstance().Print(m_Shape, (short)m_X, (short)m_Y);
+	// m_X를 2배 하는 이유는 cmd에서 X좌표 길이가 Y좌표 길이의 절반이기 때문! 
+	Console::GetInstance().Print(m_Shape, (short)(m_X * 2), (short)m_Y);
 }
 
 bool Object::Move(Direction _dir, float _distance)
 {
 	RECT boundaryBox = Console::GetInstance().GetBoundaryBox();
+
+	// cmd 좌표계인 boundaryBox를 인게임 좌표계로 변환하여 계산하도록 한다.
+	// cmd 좌표계에서는 X 길이가 Y 길이의 절반이기 때문에 인게임 좌표계산 과정에 바로 쓰기에 부적절하다.
+	float minX = (float) boundaryBox.left / 2;
+	float maxX = (float) boundaryBox.right / 2;
+	float minY = (float) boundaryBox.top;
+	float maxY = (float) boundaryBox.bottom;
+
 	switch (_dir)
 	{
 	case Direction::UP:
 		{
 			// 현재 y좌표가 위쪽 경계선 보다 크면(아래쪽이면) 위로 이동이 가능
-			if (m_Y > boundaryBox.top)
+			if (m_Y > minY)
 			{
 				// 요청한 거리만큼 이동을 시도하며, 바운더리를 넘어서지 않도록 처리한다.
 				// std::max는 두 개의 인자 중 더 큰 것을 반환하는 함수이다.
 				// m_Y - _distance가 바운더리보다 작으면 바운더리 값을 반환하여, 그 이하 값이 나오지 않도록 한다.
-				m_Y = std::max<float>(m_Y - _distance, boundaryBox.top);
+				m_Y = std::max<float>(m_Y - _distance, minY);
 
 				//여기에 왔다는 것은 이동은 하고 있다는 뜻이다.
 				return true;
@@ -89,9 +98,9 @@ bool Object::Move(Direction _dir, float _distance)
 	case Direction::DOWN:
 		{
 			// 현재 y좌표가 아래쪽 경계선 보다 작으면(위쪽이면) 아래쪽으로 이동이 가능
-			if (m_Y < boundaryBox.bottom)
+			if (m_Y < maxY)
 			{
-				m_Y = std::min<float>(m_Y + _distance, boundaryBox.bottom);
+				m_Y = std::min<float>(m_Y + _distance, maxY);
 				return true;
 			}
 		}
@@ -100,10 +109,10 @@ bool Object::Move(Direction _dir, float _distance)
 	case Direction::LEFT:
 		{
 			// 현재 x좌표가 왼쪽 경계선 보다 크면(오른쪽이면) 왼쪽으로 이동이 가능
-			if (m_X > boundaryBox.left)
+			if (m_X > minX)
 			{
 				// 거리 2배를 곱하는 이유는 x좌표가 y좌표의 절반이기 때문
-				m_X = std::max<float>(m_X - _distance * 2, boundaryBox.left);
+				m_X = std::max<float>(m_X - _distance, minX);
 				return true;
 			}
 		}
@@ -112,9 +121,9 @@ bool Object::Move(Direction _dir, float _distance)
 	case Direction::RIGHT:
 		{
 			// 현재 x좌표가 왼쪽 경계선 보다 작으면(왼쪽이면) 오른쪽으로 이동이 가능
-			if (m_X < boundaryBox.right)
+			if (m_X < maxX)
 			{
-				m_X = std::min<float>(m_X + _distance * 2, boundaryBox.right);
+				m_X = std::min<float>(m_X + _distance, maxX);
 				return true;
 			}
 		}
